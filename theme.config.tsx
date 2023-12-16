f3d3ab372e5eada61358c7ce41a6f1f6d258b536
David Nachman <david@nachman.org>
2023-12-16 14:15:20 -0500

    migrate to nextra

---
+++ theme.config.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>n5n.org</span>,
+  project: {},
+  chat: {},
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  footer: {
+    text: "n5n.org",
+  },
+};
+
+export default config;
