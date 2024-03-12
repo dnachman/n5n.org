2927b7ac90b1f49a5dfe02d16a8939462202cb0c
David Nachman <david@nachman.org>
2024-03-12 21:00:05 +0000

    many basics in place, testing not there yet

---
+++ app/api/ai/bedrock/bedrock-services.js
@@ -0,0 +1,33 @@
+
+import { Bedrock } from "@langchain/community/llms/bedrock";
+
+export async function bedrockText(query, model, maxTokens, modelOptions) {
+  // If no credentials are provided, the default credentials from
+  // @aws-sdk/credential-provider-node will be used.
+  const bedrockModel = new Bedrock.Bedrock({
+    model: model || "amazon.titan-text-express-v1", // You can also do e.g. "anthropic.claude-v2", "meta.llama2-13b-v1"
+    region: "us-east-1",
+    streaming: true,
+    // endpointUrl: "custom.amazonaws.com",
+    // credentials: {
+    //   accessKeyId: process.env.BEDROCK_AWS_ACCESS_KEY_ID!,
+    //   secretAccessKey: process.env.BEDROCK_AWS_SECRET_ACCESS_KEY!,
+    // },
+    // modelKwargs: {},
+    modelKwargs: modelOptions || {},
+    maxTokens: maxTokens || 256,
+  });
+
+  // const stream = await bedrockModel.stream(query);
+  // const chunks = [];
+  // for await (const chunk of stream) {
+  //   chunks.push(chunk);
+  //   console.log(`${chunk}|`);
+  // }
+  // return chunks.join();
+
+  const resp = await bedrockModel.invoke(query);
+  console.log("Bedrock response: ", resp);
+  return resp;
+};
+
---
+++ app/api/ai/bedrock/route.js
@@ -0,0 +1,31 @@
+"use strict";
+
+import { bedrockText } from "../../components/aws-bedrock";
+
+export async function POST(request) {
+  const prompt = request.body?.prompt || "What is the meaning of life?";
+  const model = request.body?.model || "amazon.titan-text-express-v1";
+  const maxTokens = request.body?.maxTokens || null;
+  const modelOptions = request.body?.modelOptions || null;
+
+  console.log(
+    "Calling bedrock-text: ",
+    prompt,
+    model,
+    maxTokens,
+    modelOptions
+  );
+
+  try {
+    const resp = await awsBedrock.bedrockText(prompt, model);
+
+    return Response.json(resp);
+
+  } catch (error) {
+    console.log("Error calling bedrock: ", error);
+    return new Response(`Error calling bedrock: ${error.message}`, {
+      status: 500,
+    })
+  }
+}
+
